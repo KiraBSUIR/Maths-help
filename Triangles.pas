c4ec58cea72b2f90ddbdcc5352ea233e36c675e2
KiraBSUIR <kistalmage@gmail.com>
2015-12-30 17:33:03 +0300

    changes in Triangles.pas: now its can calculate square of isosceles triangles

--- Triangles.pas
+++ Triangles.pas
@@ -3,7 +3,7 @@ unit Triangles;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  Math, System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
   FMX.Controls.Presentation, FMX.Edit;
 
@@ -19,16 +19,16 @@ type
     btnWithoutRadius: TButton;
     btnShowMenu: TButton;
     edtFirstAngle: TEdit;
-    edtSecondAngle: TEdit;
-    edtThirdAngle: TEdit;
     btnwithangles: TButton;
     btnWithoutAngles: TButton;
+    btnClearAll: TButton;
     procedure btnFindSquareClick(Sender: TObject);
     procedure btnThroughTheRadiusClick(Sender: TObject);
     procedure btnWithoutRadiusClick(Sender: TObject);
     procedure btnShowMenuClick(Sender: TObject);
     procedure btnwithanglesClick(Sender: TObject);
     procedure btnWithoutAnglesClick(Sender: TObject);
+    procedure btnClearAllClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -43,9 +43,19 @@ implementation
 
 {$R *.fmx}
 uses Menu;
+procedure TForm1.btnClearAllClick(Sender: TObject);
+begin
+   edtFirstAngle.Text := '';
+   edtFirstSide.Text := '';
+   edtSecondSide.Text := '';
+   edtThirdSide.Text := '';
+   edtRadius.Text := '';
+   LabelSquare.Text := 'Square of triangle: ';
+end;
+
 procedure TForm1.btnFindSquareClick(Sender: TObject);
 var
-   FirstSide, SecondSide, ThirdSide, Radius: Integer;
+   semiperimeter, FirstAngle, FirstSide, SecondSide, ThirdSide, Radius: Integer;
    Height, Square: real;
 begin
    if (edtFirstSide.Text <> '') then
@@ -54,28 +64,95 @@ begin
       SecondSide := StrToInt(edtSecondSide.text);
    if (edtThirdSide.Text <> '') then
       ThirdSide := StrToInt(edtThirdSide.text);
-   if (check = 0) then
+   if (edtFirstAngle.Text <> '') then
+      FirstAngle := StrToInt(edtFirstAngle.text);
+   if (edtRadius.Text <> '') then
+      Radius := StrToInt(edtRadius.text);
       if (FirstSide = SecondSide) and (FirstSide = ThirdSide) and (SecondSide = ThirdSide) then
          begin
-            Square := Trunc((Sqr(FirstSide) * Sqrt(3)) / 4);
-            LabelSquare.Text := 'square of equilateral triangle is: ' + FloatToStr(Square);
+            if (check = 0) then
+               begin
+                  Square := Trunc((Sqr(FirstSide) * Sqrt(3)) / 4);
+                  LabelSquare.Text := 'square of equilateral triangle is: ' + FloatToStr(Square);
+               end;
+            if (Check = 1) then
+               begin
+                  if (edtRadius.Text <> '') then
+                     Radius := StrToInt(edtRadius.text);
+                  Radius := Radius * Radius;
+                  Square := 3 * Sqrt(3) * Radius;
+                  LabelSquare.Text := 'square of equilateral triangle is: ' + FloatToStr(Square);
+               end;
          end;
-   if (Check = 1) then
-      begin
-         if (edtRadius.Text <> '') then
-            Radius := StrToInt(edtRadius.text);
-         Radius := Radius * Radius;
-         Square := 3 * Sqrt(3) * Radius;
-         LabelSquare.Text := 'square of equilateral triangle is: ' + FloatToStr(Square);
-      end;
-      {if (FirstSide = SecondSide) then
-      begin
-         ThirdSide := round((1 / 2) * ThirdSide);
-         height := (SecondSide * SecondSide) - (ThirdSide * ThirdSide);
-         Height := Sqrt(height);
-         Square := ThirdSide * height;
-         LabelSquare.Text := 'square of triangle is: ' + FloatToStr(Square);
-      end;       }
+      if (FirstSide = SecondSide) then
+         begin
+            if (check = 2) then
+               begin
+                  Square := trunc((1 / 2) * FirstSide * SecondSide * Sin(FirstAngle));
+                  LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+               end
+            else
+               if (check = 1) then
+                  begin
+                     semiperimeter := Trunc((FirstSide + SecondSide + ThirdSide) / 2);
+                     Square := Semiperimeter * Radius;
+                      LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+                  end
+               else
+            begin
+               ThirdSide := round((1 / 2) * ThirdSide);
+               height := (SecondSide * SecondSide) - (ThirdSide * ThirdSide);
+               Height := Sqrt(height);
+               Square := Trunc(ThirdSide * height);
+               LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+            end;
+         end;
+      if (FirstSide = ThirdSide) then
+         begin
+            if (check = 2) then
+               begin
+                  Square := trunc((1 / 2) * FirstSide * ThirdSide * Sin(FirstAngle));
+                  LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+               end
+            else
+               if (check = 1) then
+                  begin
+                     semiperimeter := Trunc((FirstSide + SecondSide + ThirdSide) / 2);
+                     Square := Semiperimeter * Radius;
+                      LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+                  end
+               else
+            begin
+               SecondSide := round((1 / 2) * SecondSide);
+               height := (FirstSide * FirstSide) - (SecondSide * SecondSide);
+               Height := Sqrt(height);
+               Square := Trunc(SecondSide * height);
+               LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+            end;
+         end;
+      if (SecondSide = ThirdSide) then
+         begin
+            if (check = 2) then
+               begin
+                  Square := trunc((1 / 2) * SecondSide * ThirdSide * Sin(FirstAngle));
+                  LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+               end
+            else
+               if (check = 1) then
+                  begin
+                     semiperimeter := Trunc((FirstSide + SecondSide + ThirdSide) / 2);
+                     Square := Semiperimeter * Radius;
+                      LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+                  end
+               else
+            begin
+               FirstSide := round((1 / 2) * FirstSide);
+               height := (ThirdSide * ThirdSide) - (FirstSide * FirstSide);
+               Height := Sqrt(height);
+               Square := Trunc(FirstSide * height);
+               LabelSquare.Text := 'square of isosceles triangle is: ' + FloatToStr(Square);
+            end;
+         end;
 end;
 
 procedure TForm1.btnShowMenuClick(Sender: TObject);
@@ -94,16 +171,12 @@ procedure TForm1.btnwithanglesClick(Sender: TObject);
 begin
    check := 2;
    edtFirstAngle.Visible := True;
-   edtSecondSide.Visible := True;
-   edtThirdAngle.Visible := true;
 end;
 
 procedure TForm1.btnWithoutAnglesClick(Sender: TObject);
 begin
    check := 0;
    edtFirstAngle.Visible := false;
-   edtSecondSide.Visible := false;
-   edtThirdAngle.Visible := false;
 end;
 
 procedure TForm1.btnWithoutRadiusClick(Sender: TObject);
